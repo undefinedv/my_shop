23f87f1c4cf452200aad27d359a99e9210061aec
undefined <shuxueyouqu@qq.com>
2017-12-15 12:04:40 +0800

    gix parent__construct

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,7 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User extends CI_Controller {
     public function __construct()
     {
+        parent::__construct();
         $this->load->library('session');
+
         $this->load->model("UserModel");
     }
 
